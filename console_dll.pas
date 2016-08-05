6e2279341dc9fb6899c0cc8823afba4a93b5a5c8
Rafal Urniaz <RafalUrniaz@users.noreply.github.com>
2016-08-05 15:26:39 +0100

    Add files via upload

---
+++ console_dll.pas
@@ -0,0 +1,38 @@
+(*
+Program name: BioTesseract™ ONE Desktop
+Version: 1.1.1 Build: 206
+Authorship: The project was initially invented and developed by Dr Rafal Urniaz, actually it is developed by BioTesseract™ ONE community.
+Description: This file is a part of the BioTesseract™ ONE Desktop project, for details please visit http://www.biotesseract.com
+*)
+
+unit Console_dll;
+
+{$mode objfpc}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+  Function AddCommunicate(communicate:string;ShowConsole:boolean):boolean;
+
+implementation
+
+Uses
+  Unit1, Unit22;
+
+Function AddCommunicate(communicate:string;ShowConsole:boolean):boolean;
+ begin
+  if ShowConsole then
+    begin
+       Mainform.PairSplitter1.Position:=Mainform.PairSplitter1.Height-200;
+    end;
+
+    Console.Memo1.Lines.add(DateTimeToStr(now)+': '+communicate);
+    Console.Memo1.SelStart:= Length(Console.Memo1.Text)+1;
+
+    result:=true;
+end;
+
+end.
+
